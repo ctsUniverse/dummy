332a0d3a29fe3eea699bb6600ec4ed2e6c4f17e4
ctsUniverse <dev@ctsu.eu>
2020-04-05 22:40:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ctsUniverse</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-900 leading-none antialiased">
+    <div id="app" class="flex justify-center items-center bg-gray-900 text-teal-700 h-screen">
+        <div>
+            <div class="flex flex-row text-6xl">
+                <div class="font-hairline">cts</div>
+                <div class="">Universe</div>        
+            </div>
+            <div class="text-center text-sm italic font-light">
+                Coming later this year, probably. 
+            </div>
+        </div>
+    </div>
+</body>
+</html>
